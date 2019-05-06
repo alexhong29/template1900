26e08a68f5fde5c5982ace4d75d2736f45baf311
Minh Thuan <minhthuansh@gmail.com>
2019-05-06 22:15:32 +0700

    fix tablinks
    fix tablink 1905

--- js/1905.js
+++ js/1905.js
@@ -1,11 +1,11 @@
 function openCity(evt, cityName) {
   // Declare all variables
-  var i, tabcontent, tablinks1905;
+  var i, tabcontents1905, tablinks1905;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  tabcontents1905 = document.getElementsByClassName("tabcontents");
+  for (i = 0; i < tabcontents1905.length; i++) {
+    tabcontents1905[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
