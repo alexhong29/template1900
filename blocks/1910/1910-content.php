1cc321fbc42bb03aaa83464dbe2a5a61fb5e634b
quockhanh973 <33076943+quockhanh973@users.noreply.github.com>
2019-05-07 01:14:52 +0700

    up

--- blocks/1910/1910-content.php
+++ blocks/1910/1910-content.php
@@ -171,7 +171,6 @@ $url_path = str_replace('\\', '/', $url_path);
                                 <option value="price">Sort by price: low to high</option>
                                 <option value="price-desc">Sort by price: high to low</option>
                             </select>
-                            <span><img class="icon-img" src="<?php echo $url_path ?>/images/img1.PNG" alt="#"></span>
                         </form>
                     </div>
                 </div>
