8c77c7a16586b420a605ac8bf72c30565d4488f5
Minh Thuan <minhthuansh@gmail.com>
2019-05-07 10:37:35 +0700

    update

--- js/1906.js
+++ js/1906.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         loop: true,
         calculateHeight: true,
         mode: 'horizontal',
+        
         navigation: {
             nextEl: '.next-arroww',
             prevEl: '.prev-arroww',
