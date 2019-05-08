377ca8bda96ac427a4893355da7a05358ad647a9
quockhanh973 <33076943+quockhanh973@users.noreply.github.com>
2019-05-08 10:23:09 +0700

    up

--- blocks/1908/js/1908.js
+++ blocks/1908/js/1908.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $("#slider-range1").slider({
         range: true,
         min: 26,
-        max: 700,
-        values: [26, 700],
+        max: 899,
+        values: [26, 899],
         slide: function (event, ui) {
             $("#amount").val(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
         }
