19a463f0cf240c099c282e5031b2547443662f3b
quockhanh973 <33076943+quockhanh973@users.noreply.github.com>
2019-06-04 15:02:16 +0700

    update module

---
+++ blocks/1905/1905.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/1905.less', 'css/1905.css');
+?>
+<html lang="en">
+    <head>
+        <!-- head -->
+        <title>1905</title>
+        <meta charset="utf-8">
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Basic usage demo">
+        <meta name="author" content="David Deutsch">
+        <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/1905.js"></script>
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/1905.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- <script type="js/1905.js"></script> -->
+    </head>
+    <body>
+        <?php include '../1905/1905-content.php'; ?>
+    </body>
+</html>
