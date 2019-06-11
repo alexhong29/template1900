c0d742b8d5ab71149e95c2a43ae3161abbdba034
quockhanh973 <33076943+quockhanh973@users.noreply.github.com>
2019-06-11 10:30:27 +0700

    up

--- blocks/1900/js/1900.js
+++ blocks/1900/js/1900.js
@@ -78,12 +78,19 @@ $(document).ready(function () {
         $('.active').css({"background": "none", "color": "#666"});
         $('#collap').css({"display": "none"});
     });
-    $('.btn-collapse').click(function () {
-        $('#collap').css({"display": "inline-table"});
-    });
+//    $('.btn-collapse').click(function () {
+//        $('#collap').css({"display": "inline-table"});
+//    });
     $('.active').click(function () {
         $('.active').css({"background": "#fe9c00", "color": "#fff"});
         $('#collap').css({"display": "none"});
+    });
+    $('.btn-collapse').click(function () {
+        $(this).children('ul').stop(true, true).slideDown(200);
+        $(this).toggleClass('open');
+    }, function () {
+        $(this).children('ul').stop(true, true).slideUp(200);
+        $(this).toggleClass('open');
     });
 });
 
